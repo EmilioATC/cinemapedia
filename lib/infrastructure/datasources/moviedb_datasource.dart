253920b23c5f520d75dcb50ae48cf5475645994b
EmilioATC <142368988+EmilioATC@users.noreply.github.com>
2025-01-23 18:56:24 -0600

    Implemente la obtención de datos de películas y componentes de UI con la integración de Riverpod

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -15,7 +15,7 @@ class MoviedbDatasource extends MoviesDatasource {
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    final response = await dio.get('movie/now_playing');
+    final response = await dio.get('/movie/now_playing');
     final moviesDBResponse = MovieDbResponse.fromJson(response.data);
 
     final List<Movie> movies = moviesDBResponse.results
